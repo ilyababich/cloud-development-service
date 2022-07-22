9e156d9b359823d0913b57bef75bd727f67be41d
ilyababich <ilya_babich@epam.com>
2022-07-22 13:30:49 +0300

    fix documentation

--- product-service/src/functions/createProduct/documentation.ts
+++ product-service/src/functions/createProduct/documentation.ts
@@ -1,11 +1,15 @@
 const documentation = {
-    summary: 'getProductById',
-    description: 'Get product by id',
+    summary: 'createProduct',
+    description: 'creates product',
     bodyType: 'CreateProduct',
     responseData: {
         200: {
             description: 'Ok',
             bodyType: 'GetProductsList',
+        },
+        400: {
+            description: 'Invalid request body',
+            bodyType: 'CommonFailed',
         },
         500: {
             description: 'Something went wrong!',
